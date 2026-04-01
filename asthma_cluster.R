8d351a026cdfb1a23cd6cdda70b21129429d0208
pablojimenezferia <pablojimenez@lasallistas.org.mx>
2026-04-01 11:52:25 -0600

    Add files via upload

---
+++ asthma_cluster.R
@@ -0,0 +1,132 @@
+# Script to perform cluster analysis on asthma patients
+
+library(ConsensusClusterPlus)
+
+
+# 1.- Selecting top genes by variance -------------------------------------
+
+# 1.1 Apply variance to the object of asthma only patients corrected by batch effect
+
+var_genes <- apply(expr_asthma.batch, 1, var)
+
+# 1.2 Sort in decreasing manner based on variance and select top 5000 genes
+
+top_genes <- names(sort(var_genes, decreasing = TRUE))[1:10000]
+
+# 1.3 Asign to object that will be the argument for the clustering
+
+cluster_counts <- expr_asthma.batch[top_genes, ]
+
+# 1.3.2 Convert to matrix
+
+count_matrix <- as.matrix(cluster_counts)
+
+
+# 2.- Clustering ----------------------------------------------------------
+
+# 2.1 Clustering
+
+results <- ConsensusClusterPlus(
+  count_matrix, # Objetoa procesar, debe ser una matriz
+  maxK = 5, # Numero maximo de clusters
+  reps = 1000, # Cuantas veces se repetira la funcipon
+  pItem = 0.8, # Cuantos items (1 = 100%)
+  pFeature = 1, # Cuantos features (1 = 100%) 
+  title = "Results/Cluster/", # A donde se van a guardar las imagenes
+  clusterAlg = "pam", # Cluster jerarquico
+  distance = "pearson",
+  seed = 1262118388.71279,
+  plot = "png"
+)
+
+
+# 2.2 Observe object
+
+results
+
+# 2.3 ntegretad classification likelyhood
+
+icl = calcICL(results, title = "Results/Cluster/", plot = "png")
+icl$clusterConsensus
+
+# 2.3.2 Convert to data frame to aggregate
+
+icl_df <- as.data.frame(icl$clusterConsensus)
+
+# 2.3.3 Observe the highest mean for k in icl
+
+aggregate(data = icl_df, clusterConsensus ~ k, FUN = mean)
+
+
+# 2.4 Convert the results cluster object corresponding to selected k to a column in the metadata
+
+metadata_asthma <- 
+  as.data.frame(results[[3]]$consensusClass) %>% # Data frame corresponding to which cluster does each patient belong to
+  rename(cluster = "results[[3]]$consensusClass") %>% # Rename column to "cluster"
+  rownames_to_column("file_name") %>% # Asign rownames to a column for let join
+  left_join(x = metadata_asthma, by = "file_name") %>% # Join
+  column_to_rownames("file_name") # Rename rownames
+
+
+# PCA of expression
+
+pca_asthma <- prcomp(t(expr_asthma.batch))
+
+eigen_as <- get_eigenvalue(pca_asthma)
+
+
+pca_plot_as <- function(i, u){
+  
+  df2 <- data.frame(
+    PCi = pca_asthma$x[,i],
+    PCu = pca_asthma$x[,u],
+    th2 = metadata_asthma$th2_group,
+    cluster = factor(metadata_asthma$cluster),
+    age = metadata_asthma$age.bin
+  )
+  
+  p <- ggplot(df2, aes(PCi, PCu, color = th2, shape = age)) +
+    geom_point(size = 3)  +
+    stat_ellipse(geom = "polygon", 
+                 aes(fill = cluster, group = cluster), 
+                 alpha = 0.2, 
+                 level = 0.9,
+                 color = NA) +
+    scale_fill_brewer(palette = "Set2") +
+    labs(x = paste0("PC", i, " (", round(eigen_as[i, 2], 2), "%)"),
+         y = paste0("PC", u, " (", round(eigen_as[u, 2], 2), "%)")
+    )
+  print(p)
+}
+
+pca_plot_as(1, 2)
+
+for (i in 1:5) {
+  pca_plot_as(i, i + 1)
+}
+
+
+
+
+# 6.5.2 Top contributors to PC1 and PC2
+
+a <- fviz_contrib(pca_asthma, choice = "var", axes = 1, top = 30)
+
+
+b <- fviz_contrib(pca_asthma, choice = "var", axes = 2, top = 30)
+
+
+grid.arrange(a, b, ncol = 2)
+
+
+# Observe the distribution of the original publications TH2 classification
+# within our clusters
+
+metadata_asthma %>% 
+  filter(.preserve = c(th2_group, cluster)) %>% 
+  count(th2_group, cluster)
+
+
+prop.table(table(metadata_asthma$cluster, metadata_asthma$th2_group), 1)
+
+chisq.test(table(metadata_asthma$cluster, metadata_asthma$th2_group))
